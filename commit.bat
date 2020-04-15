babd843dd75efce30f4cdf205226bc929339c26d
takala <treasure8800@gmail.com>
2020-04-15 20:24:23 +0900

    no message

--- commit.bat
+++
@@ -1,3 +0,0 @@
-START /WAIT git add -A
-START /WAIT git commit -a -m ""
-START /WAIT git push origin master
--- generate.bat
+++ generate.bat
@@ -1,3 +1,6 @@
-START /WAIT python generate.py
+START /WAIT python change.py
 
-.\diary\%date:~0,4%%date:~5,2%%date:~8,2%.html
+START /WAIT pelican
+
+pmake html
+
